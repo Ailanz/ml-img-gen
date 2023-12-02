2852464e3775cc64860f0b7fa4e5c00d24ff071f
Andrew Tu <andrewtu@outlook.com>
2023-12-01 23:27:11 -0500

    wip

--- src/evaluation/EvaluateDenoiser.py
+++ src/evaluation/EvaluateDenoiser.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 class EvaluateDenoiser(Callback):
-    def __init__(self, evaluate_epochs=10, steps=13, model=None, shape=(128, 128, 3)):
+    def __init__(self, evaluate_epochs=10, steps=10, model=None, shape=(128, 128, 3)):
         super(EvaluateDenoiser, self).__init__()
         self.evaluate_epochs = evaluate_epochs
         self.model = model
@@ -20,18 +20,19 @@ class EvaluateDenoiser(Callback):
         if (epoch + 1) % self.evaluate_epochs == 0:
             try:
                 samples = 5
-                divisor = 4
+                divisor = 2
 
                 noisy_img = np.random.normal(0.5, size=(samples, *self.shape))
                 noisy_img = np.clip(noisy_img, 0., 1.)
                 imgs = [noisy_img]
                 for i in range(self.steps):
-                    y_pred = self.model.predict([noisy_img, np.repeat(0.1, noisy_img.shape[0], axis=0)], verbose=0)
+                    # y_pred = self.model.predict([noisy_img, np.repeat(0.1, noisy_img.shape[0], axis=0)], verbose=0)
+                    y_pred = self.model.predict(noisy_img, verbose=0)
                     noisy_img = noisy_img - y_pred
                     noisy_img = np.clip(noisy_img, 0., 1.)
                     imgs.append(noisy_img)
 
-                f, axarr = plt.subplots(samples, int(self.steps / divisor) + 1, figsize=(12, 12))
+                f, axarr = plt.subplots(samples, int(self.steps / divisor), figsize=(12, 12))
 
                 for i in range(samples):
                     y_pos = 0
--- src/train/denoiser.py
+++ src/train/denoiser.py
@@ -10,8 +10,10 @@ from src.model import noise_predictor
 import numpy as np
 
 
-def add_noise(images, noise_factor=0.1):
-    noisy_images = images + noise_factor * np.random.normal(loc=0., scale=1., size=images.shape)
+def add_noise(images, noise_factor=random.random()):
+    # noisy_images = images + noise_factor * np.random.normal(loc=0., scale=1., size=images.shape)
+    rand_scale = np.random.rand(images.shape[0])
+    noisy_images = images + np.random.rand(images.shape[0], 128, 128, 3) * np.random.normal(loc=0., scale=1., size=images.shape)
     noisy_images = np.clip(noisy_images, 0., 1.)
     noise = noisy_images - images
     # rand_sample = random.randint(0, images.shape[0] - 1)
@@ -28,6 +30,7 @@ def plot_img(noisy_img, img, noise):
     axarr[1].axis('off')
     axarr[2].axis('off')
     axarr[3].axis('off')
+    plt.tight_layout()
     plt.show()
 
 
@@ -44,17 +47,22 @@ if __name__ == '__main__':
     evaluate_denoiser = EvaluateDenoiser(evaluate_epochs=epoch_per_step, model=model)
     early_stopping = EarlyStopping(monitor='loss', patience=20, restore_best_weights=True)
 
-    for i in range(1000):
-        for step in range(10):
-            noise_factor = (step + 1) / 10  # Gradually increase noise factor
-            noise_factor = 0.1  # Gradually increase noise factor
-            print(f"Training step {i+1}/1000", f"noise factor: {noise_factor}")
-            noisy_images, noises = add_noise(imgs, noise_factor=noise_factor)
+    for i in range(10000):
+        noisy_images = imgs
+        for step in range(1):
+            # noise_factor = (step + 1) / 10  # Gradually increase noise factor
+            # noise_factor = 1.0  # Gradually increase noise factor
+            # print(f"Training step {i+1}/1000", f"noise factor: {noise_factor}")
+            # noisy_images, noises = add_noise(noisy_images, noise_factor=noise_factor)
+            noisy_images, noises = add_noise(imgs)
 
             # y = model.predict(np.expand_dims(noisy_images[0], axis=0))
             # plot_img(noisy_images[0], imgs[0], noisy_images[0] - y[0])
-            noise_factor_batch = np.repeat(noise_factor, noisy_images.shape[0], axis=0)
-            model.fit([noisy_images, noise_factor_batch], noises, epochs=epoch_per_step, batch_size=32, verbose=1, callbacks=[early_stopping, evaluate_denoiser])
+            # noise_factor_batch = np.repeat(noise_factor, noisy_images.shape[0], axis=0)
+            # model.fit([noisy_images, noise_factor_batch], noises, epochs=epoch_per_step, batch_size=32, verbose=1, callbacks=[early_stopping, evaluate_denoiser])
+            rand_sample = random.randint(0, imgs.shape[0] - 1)
+            plot_img(noisy_images[rand_sample], imgs[rand_sample], model.predict(np.expand_dims(noisy_images[rand_sample], axis=0))[0])
+            model.fit(noisy_images, noises, epochs=epoch_per_step, batch_size=32, verbose=1, callbacks=[early_stopping, evaluate_denoiser])
 
     model.save('denoiser.h5')
     # time_steps = 5
